fb3a6707bfaf97feda7d98660e627d6562f8432e
Gregg <greggpollack@gmail.com>
2017-11-10 10:02:47 -0500

    updating version of node

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
 var port = process.env.PORT || 5000;
 app.listen(port);
